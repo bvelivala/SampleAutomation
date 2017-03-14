15004712cf6b20b047a4c3857afdb93b7975e27a
bvelivala <bhavaniv448@gmail.com>
2017-03-14 22:59:51 +0530

    Update HomePageTest.java

--- src/test/java/org/cdsdemo/testautomation/regression/HomePageTest.java
+++ src/test/java/org/cdsdemo/testautomation/regression/HomePageTest.java
@@ -3,6 +3,7 @@ package org.cdsdemo.testautomation.regression;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
@@ -13,24 +14,41 @@ import org.testng.annotations.Test;
 
 public class HomePageTest {
 	
-	public RemoteWebDriver driver;
+	public RemoteWebDriver rdriver;
+	public WebDriver driver;
+	public boolean isRemoteDriver = true;
 	
-	@Parameters({ "remoteDriverUrl" })
+	@Parameters({ "remoteDriverUrl", "isRemote" })
 	@BeforeClass
 	public void setUp(String remoteDriverURL) throws MalformedURLException {
 		System.out.println("Remote Driver URL :: "+remoteDriverURL);
 		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-		driver = new RemoteWebDriver(new URL(remoteDriverURL), capabilities);
-		System.out.println("Driver initialized : "+driver);
-		driver.manage().window().maximize();
+		
+		if ("Y".equalsIgnoreCase(isRemote)) {
+			rdriver = new RemoteWebDriver(new URL(remoteDriverURL), capabilities);
+			System.out.println("Remote Driver initialized : "+driver);
+			rdriver.manage().window().maximize();
+		} else {
+			driver = new RemoteWebDriver(new URL(remoteDriverURL), capabilities);
+			System.out.println("Web Driver initialized : "+driver);
+			driver.manage().window().maximize();
+		}
 	}
 	
 	@Parameters({ "appUrl" })
         @Test
 	public void testDriverCurrentUrl(String appUrl) {
 		System.out.println("*** Navigation to Application ***"+appUrl);
-		driver.navigate().to(appUrl);
-		String url = driver.getCurrentUrl();	
+		String url = "";
+		
+		if (isRemoteDriver) {
+			rdriver.navigate().to(appUrl);
+			url = rdriver.getCurrentUrl();
+		} else {
+			driver.navigate().to(appUrl);
+			url = driver.getCurrentUrl();
+		}
+		
 	  	System.out.println("Driver current url is "+url);
 		Assert.assertTrue(url.contains("autoclaim"), "Driver's Current Url doesn't match");
 	}
@@ -38,15 +56,27 @@ public class HomePageTest {
 	@Parameters({ "appUrl" })
 	@Test
 	public void testHomePageTitle(String appUrl) {
-		driver.navigate().to(appUrl);
-		String strPageTitle = driver.getTitle();
+		String strPageTitle = "";
+		
+		if (isRemoteDriver) {
+			rdriver.navigate().to(appUrl);
+			strPageTitle = rdriver.getTitle();
+		} else {
+			driver.navigate().to(appUrl);
+			strPageTitle = driver.getTitle();
+		}
+		
 		System.out.println("*** Verifying page title ***");
 		Assert.assertTrue(strPageTitle.equalsIgnoreCase("Login Page"), "Page title doesn't match");
 	}
 
         @Test
         public void testDriverPageSource() {
-                Assert.assertTrue(driver.getPageSource().contains("Register User"), "Driver's page source doesn't match");
+		if (isRemoteDriver){ 
+                	Assert.assertTrue(rdriver.getPageSource().contains("Register User"), "Driver's page source doesn't match");
+		} else {
+			Assert.assertTrue(driver.getPageSource().contains("Register User"), "Driver's page source doesn't match");
+		}
         }
 
 	
@@ -54,6 +84,9 @@ public class HomePageTest {
 	public void closeBrowser() {
 		if (driver != null) {
 			driver.quit();
+		}
+		if (rdriver != null) {
+			rdriver.quit();
 		}
 	}
 }
