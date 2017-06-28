a75154e0406dadc9fe5316c804c8f474ab5cc972
bvelivala <bhavaniv448@gmail.com>
2017-06-28 15:03:22 +0530

    firefox

--- src/test/java/org/cdsdemo/testautomation/regression/HomePageTest.java
+++ src/test/java/org/cdsdemo/testautomation/regression/HomePageTest.java
@@ -20,7 +20,7 @@ public class HomePageTest {
 	@BeforeClass
 	public void setUp(String remoteDriverURL) throws MalformedURLException {
 		System.out.println("Remote Driver URL :: "+remoteDriverURL);
-		DesiredCapabilities capabilities = DesiredCapabilities.chrome();		
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();		
 		rdriver = new RemoteWebDriver(new URL(remoteDriverURL), capabilities);
 		System.out.println("Remote Driver initialized : "+rdriver);
 		rdriver.manage().window().maximize();
